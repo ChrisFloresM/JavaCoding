ad7a607fd2204e530be4038769e682a24b013419
Christian <christian.mig.flmo@gmail.com>
2023-07-13 02:10:07 -0600

    Adding excercise 1 from Basic Java - Open Bootcamp

---
+++ OB_JavaBasico/Excercise_module_2/src/main.java
@@ -0,0 +1,24 @@
+public class main {
+    public static void main(String[] args) {
+        byte byteNumber = 25;
+        short shortNumber = 3500;
+        int intNumber = 35000;
+        long longNumber = 350000;
+
+        float floatNumber = 1.0f;
+        double doubleNumber = 1.0d;
+
+        boolean booleanNumber = false;
+        String stringNumber = "35";
+
+        System.out.println(byteNumber);
+        System.out.println(shortNumber);
+        System.out.println(intNumber);
+        System.out.println(longNumber);
+        System.out.println(floatNumber);
+        System.out.println(doubleNumber);
+        System.out.println(booleanNumber);
+        System.out.println(stringNumber);
+
+    }
+}
